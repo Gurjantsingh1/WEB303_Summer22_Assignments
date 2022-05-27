ec1bbfa5361f972848630e2af8173cff1f922068
Gurjantsingh1 <W0782841@myscc.ca>
2022-05-26 22:08:28 -0400

    done with half assignment

--- Assignement 1/README.md
+++ Assignement 1/README.md
@@ -1,2 +1,7 @@
 # WEB303_Summer22_Assignments
  Assignments
+26 may 2022
+After setting up all folders and limk with git hub now i am ready to do my assignment 1
+26 may 22  
+7:54
+As asked in instruction i added event handler 
--- Assignement 1/script.js
+++ Assignement 1/script.js
@@ -3,11 +3,17 @@
 	{Gurjant Singh}
 */
 
-
-$(document).ready(function(){
-	$("#income,#wealth").keyup(function(){
-		var tax = (($("#income").val())*0.35)+(($("#wealth").val())*0.25)
-		$("#tax").val(tax);
-	});   
-
+$(document).ready(function() {
+	$("label").keyup(function() {
+		let getPrice = $("#price").val();
+		let tax = getPrice * 0.13;
+		$("#tax").text("$" + tax.toFixed(2));
+		console.log("tax", tax)
+		$("add-price").onclick(function(){
+			let addPrice= tax + getPrice
+			console.log("price" , addPrice)
+			$("#price-list").text(addPrice)
+		})
+	});
+	
 });
