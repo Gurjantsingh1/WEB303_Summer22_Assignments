a0e468cb3b02c128753793dd4adc3e5b5bf4244f
Gurjantsingh1 <W0782841@myscc.ca>
2022-06-11 20:23:59 -0400

    add json data using 1st method

--- Assignment 3/js/script.js
+++ Assignment 3/js/script.js
@@ -1,2 +1,14 @@
 // WEB303 Assignment 3
 // Gurjant Singh
+
+     function getDataByJson(){
+        $.getJSON("data/glitchTeam.json").done(function(member){
+        $.each(member.members,function(key,val){
+     $('div#glitch-team').append(`<h4>${val.fullname}</h5
+            <h6>${val.position}</h6>
+            <p>${val.shortbio}</p>`)})
+        })
+    };
+    $(function(){
+        getDataByJson();
+    })
