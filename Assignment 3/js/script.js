e4d1c5d6a1463ac5bedbac65357e464be71728c8
Gurjantsingh1 <W0782841@myscc.ca>
2022-06-11 20:41:07 -0400

    create ajax method

--- Assignment 3/js/script.js
+++ Assignment 3/js/script.js
@@ -9,6 +9,27 @@
             <p>${val.shortbio}</p>`)})
         })
     };
+    function getDataByAjax(){
+        $.ajax({
+            url:"data/glitchTeam.json",
+            beforeSend:function(){
+                $('#glitch-team').text("Loading.....")
+            },
+            error:function(){
+                alert("Data Invaild")
+
+            },
+            success:function(){
+                $.getJSON("data/glitchTeam.json").done(function(member){
+                    $.each(member.members,function(key,val){
+                 $('div#glitch-team').append(`<h4>${val.fullname}</h5
+                        <h6>${val.position}</h6>
+                        <p>${val.shortbio}</p>`)})
+                    })
+
+            }
+        })
+    }
     $(function(){
-        getDataByJson();
-    })
+        //getDataByJson();
+getDataByAjax();    })
