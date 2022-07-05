0e4f1155abdfdfa5e904329304b90e4edb10c5b8
Gurjantsingh1 <W0782841@myscc.ca>
2022-07-05 18:42:47 -0400

    Adding javascript for accordion and tab panel in script.js

--- Assignment 4/js/script.js
+++ Assignment 4/js/script.js
@@ -1,3 +1,39 @@
+
+
 /*
- {Your name goes here}
+ {Gurjant singh}
 */
+$(function () {
+    $(".accordion").on("click", ".accordion-control", function (e) {
+      e.preventDefault();
+      $(this).next(".accordion-panel").not(":animated").slideToggle();
+    });
+    $(".tab-list").each(function () {
+        
+        var $this = $(this); 
+        var $activeTab = $this.find("li.active"); 
+        var $activeTabLink = $activeTab.find("a"); 
+        var $activePanelId = $activeTabLink.attr("href"); 
+        var $activePanel = $($activePanelId);
+       
+        $this.on("click", ".tab-control", function (e) {
+         
+          e.preventDefault(); 
+          var $clickedTabLink = $(this); 
+          var clickedPanelId = this.hash; 
+          if (clickedPanelId && !$clickedTabLink.parent().is(".active")) {
+            $activePanel.removeClass("active"); 
+            $activeTab.removeClass("active"); 
+            $activePanel = $(clickedPanelId).addClass("active"); 
+           
+           
+            
+    
+            $activeTab = $clickedTabLink.parent().addClass("active");
+          }
+        });
+      });
+  });
+
+
+  
